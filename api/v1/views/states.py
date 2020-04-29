f4e61f1713942b1337a6e08c18857a2c01696a03
francisco0522 <pacho0522@gmail.com>
2020-04-29 14:57:43 -0500

    Fixing states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -59,7 +59,7 @@ def create_state():
         abort(400, 'Not a JSON')
     if 'name' not in info:
         abort(400, 'Missing name')
-    state = State(name=info['name'])
+    state = State(**info)
     storage.new(state)
     storage.save()
     return jsonify(state.to_dict()), 201
