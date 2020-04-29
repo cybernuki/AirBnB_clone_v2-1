9cbcc01e21e2e3332b254d0ac599e455fc3359cc
francisco0522 <pacho0522@gmail.com>
2020-04-29 13:10:55 -0500

    Fixing task 9

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -66,7 +66,7 @@ def create_amenity():
     return jsonify(amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id >', strict_slashes=False,
+@app_views.route('/amenities/<amenity_id>', strict_slashes=False,
                  methods=['PUT'])
 def modify_amenity(amenity_id):
     """ An endpoint that modifies a specific
