72ec8f341a3106d77843e95c15d0dda5fd450c1c
cybernuki <1164@holbertonschool.com>
2020-04-29 17:51:59 +0000

    implement amenity blue print

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,5 +6,5 @@ app_views = Blueprint('status', __name__, url_prefix='/api/v1')
 
 from api.v1.views import index
 from api.v1.views.states import *
-# from api.v1.views.amenities import *
+from api.v1.views.amenities import *
 from api.v1.views.cities import *
